918ca0160e8c3f662492abc27ba3edbc0f95e15d
artimZzz <artim1983@hotmail.com>
2025-05-26 12:20:22 +0800

    feat: server side component

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,19 +3,21 @@ import { UserSchema } from '@/schema/user'
 import type { User } from '@/type/user'
 import type { UseSuspenseQueryOptions } from '@tanstack/react-query'
 
+export async function fetchData(): Promise<User[]> {
+  const apiURL = `${API_DOMAIN}${ApiPath.User}`
+
+  const response = await fetch(apiURL)
+
+  const responseJson = await response.json()
+
+  const parsedData = responseJson.map(UserSchema.parse)
+
+  return parsedData
+}
+
 export const API_GET_USER: UseSuspenseQueryOptions<User[]> = {
   queryKey: [ApiPath.User],
-  queryFn: async function fetchData() {
-    const apiURL = `${API_DOMAIN}${ApiPath.User}`
-
-    const response = await fetch(apiURL)
-
-    const responseJson = await response.json()
-
-    const parsedData = responseJson.map(UserSchema.parse)
-
-    return parsedData
-  },
+  queryFn: fetchData,
   staleTime: 1000 * 60, // 1 minute
   gcTime: 1000 * 60 * 5 // 5 minutes
 }
